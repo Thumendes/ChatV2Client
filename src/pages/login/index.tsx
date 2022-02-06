738c20e46d288f943d09435d783e0806a57acce4
Arthur Mendes <thumendess@gmail.com>
2022-02-05 23:23:19 -0300

    Change imports

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,8 @@
 import { Center, Flex, Text } from "@chakra-ui/react";
 import Layout from "components/Layout";
 import type { NextPage } from "next";
-import { LoginForm } from "./_login";
-import { RegisterForm } from "./_register";
+import LoginForm from "./_login";
+import RegisterForm from "./_register";
 
 const Home: NextPage = () => {
   return (
